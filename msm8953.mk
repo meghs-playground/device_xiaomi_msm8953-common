bb2eaf5368eeaa7a30ea4811817130b163abc82d
Meghthedev <meghthedev@gmail.com>
2024-04-10 10:51:24 +0530

    msm8953-common: remove packages
    * gapps are too bloated, and our partition too small.
    
    It fixes:  Out of space? Out of inodes? The tree size of /tmp/src/android/out/soong/.temp/tmpsvgt6q6g is 3954970624 bytes (3771 MB), with reserved space of 0 bytes (0 MB).
    The max image size for filesystem files is 3221225472 bytes (3072 MB), out of a total partition size of 3221225472 bytes (3072 MB).

--- msm8953.mk
+++ msm8953.mk
@@ -419,5 +419,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Remove packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
